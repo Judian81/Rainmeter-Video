fa2bf4bff3a97afe1105c03c90aff7febe09ce82
Judian81 <46109964+Judian81@users.noreply.github.com>
2021-07-27 02:33:08 +0200

    Add project files.

---
+++ Rainmeter-Video/Form1.Designer.vb
@@ -0,0 +1,71 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'AxWindowsMediaPlayer1
+        '
+        Me.AxWindowsMediaPlayer1.Enabled = True
+        Me.AxWindowsMediaPlayer1.Location = New System.Drawing.Point(0, 0)
+        Me.AxWindowsMediaPlayer1.Name = "AxWindowsMediaPlayer1"
+        Me.AxWindowsMediaPlayer1.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWindowsMediaPlayer1.Size = New System.Drawing.Size(188, 188)
+        Me.AxWindowsMediaPlayer1.TabIndex = 0
+        Me.AxWindowsMediaPlayer1.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.LightSalmon
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(188, 188)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 11
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(271, 188)
+        Me.Controls.Add(Me.AxWindowsMediaPlayer1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Rainmeter-Video/Form1.vb
@@ -0,0 +1,296 @@
+﻿Imports System
+Imports System.Runtime.InteropServices
+Imports System.Diagnostics
+Imports Microsoft.VisualBasic.ApplicationServices
+
+Public Class Form1
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        'when the application is closing, remove the handler to the Application`s StartupNextInstance event
+        RemoveHandler My.Application.StartupNextInstance, AddressOf Me_StartupNextInstance
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'add a handler to the Application`s StartupNextInstance event
+        AddHandler My.Application.StartupNextInstance, AddressOf Me_StartupNextInstance
+        'get the arguments passed to the applications command line arguments. This will be the filename of the
+        'file that was double clicked which will open the first instance of your program.
+        Dim argcount As Integer, url As String, position As Integer, positiontemp As String, pause As String, duration As String
+        duration = ""
+        pause = ""
+        argcount = 0
+        url = ""
+        positiontemp = ""
+        position = 0
+        AxWindowsMediaPlayer1.settings.mute = True
+        AxWindowsMediaPlayer1.settings.volume = 0
+        If My.Application.CommandLineArgs.Count > 0 Then
+            For Each arg As String In My.Application.CommandLineArgs
+                argcount += 1
+                Select Case argcount
+                    Case 1
+                        url = arg
+                        If Strings.Right(url, 4) = ".3gp" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 3) = ".aa" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".aac" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".aax" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".act" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 5) = ".aiff" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 5) = ".alac" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".amr" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".ape" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 3) = ".au" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".awb" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".dss" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".dvf" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 5) = ".flac" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".gsm" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 6) = ".iklax" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".ivs" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".m4a" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".m4b" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".m4p" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".mmf" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".mp3" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".mpc" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".msv" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".nmf" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".ogg" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".oga" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 5) = ".mogg" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 5) = ".opus" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 3) = ".ra" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 3) = ".rm" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".raw" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 5) = ".rf64" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".sln" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".tta" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".voc" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".vox" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".wav" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".wma" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 3) = ".wv" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 5) = ".webm" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 5) = ".8svx" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".cda" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        Else
+                            AxWindowsMediaPlayer1.Visible = True
+                        End If
+                    Case 2
+                        positiontemp = arg
+                        position = Split(positiontemp, ":")(0) * 60 + Split(positiontemp, ":")(1)
+                    Case 3
+                        Me.Left = arg + 10
+                        AxWindowsMediaPlayer1.Left = 0
+                    Case 4
+                        Me.Top = arg + 193
+                        AxWindowsMediaPlayer1.Top = 0
+                    Case 5
+                        Me.Width = arg
+                        AxWindowsMediaPlayer1.Width = Me.Width
+                    Case 6
+                        Me.Height = arg
+                        AxWindowsMediaPlayer1.Height = Me.Height
+                    Case 7
+                        pause = arg
+                    Case 8
+                        Me.PictureBox1.ImageLocation = arg
+                    Case 9
+                        duration = arg
+                        If pause = "Pause.png" And duration <> "0:00" Then
+                            AxWindowsMediaPlayer1.URL = url
+                            AxWindowsMediaPlayer1.Ctlcontrols.currentPosition = position
+                            AxWindowsMediaPlayer1.Ctlcontrols.play()
+                        Else
+                            AxWindowsMediaPlayer1.Ctlcontrols.pause()
+                        End If
+                End Select
+            Next
+        End If
+    End Sub
+
+    'this is the handler for the Application`s StartupNextInstance event
+    Private Sub Me_StartupNextInstance(ByVal sender As Object, ByVal e As StartupNextInstanceEventArgs)
+        'get the command line arguments that where passed to this application when the user double clicks on
+        'a file and your application is already opened.
+        Dim argcount As Integer, url As String, position As Integer, positiontemp As String, pause As String, duration As String
+        duration = ""
+        pause = ""
+        argcount = 0
+        url = ""
+        positiontemp = ""
+        position = 0
+        If e.CommandLine.Count > 0 Then
+            For Each arg As String In e.CommandLine
+                argcount += 1
+                Select Case argcount
+                    Case 1
+                        url = arg
+                        If Strings.Right(url, 4) = ".3gp" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 3) = ".aa" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".aac" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".aax" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".act" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 5) = ".aiff" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 5) = ".alac" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".amr" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".ape" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 3) = ".au" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".awb" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".dss" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".dvf" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 5) = ".flac" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".gsm" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 6) = ".iklax" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".ivs" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".m4a" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".m4b" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".m4p" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".mmf" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".mp3" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".mpc" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".msv" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".nmf" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".ogg" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".oga" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 5) = ".mogg" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 5) = ".opus" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 3) = ".ra" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 3) = ".rm" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".raw" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 5) = ".rf64" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".sln" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".tta" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".voc" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".vox" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".wav" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".wma" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 3) = ".wv" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 5) = ".webm" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 5) = ".8svx" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        ElseIf Strings.Right(url, 4) = ".cda" Then
+                            AxWindowsMediaPlayer1.Visible = False
+                        Else
+                            AxWindowsMediaPlayer1.Visible = True
+                        End If
+                    Case 2
+                        positiontemp = arg
+                        position = Split(positiontemp, ":")(0) * 60 + Split(positiontemp, ":")(1)
+                    Case 3
+                        Me.Left = arg + 10
+                        AxWindowsMediaPlayer1.Left = 0
+                    Case 4
+                        Me.Top = arg + 193
+                        AxWindowsMediaPlayer1.Top = 0
+                    Case 5
+                        Me.Width = arg
+                        AxWindowsMediaPlayer1.Width = Me.Width
+                    Case 6
+                        Me.Height = arg
+                        AxWindowsMediaPlayer1.Height = Me.Height
+                    Case 7
+                        pause = arg
+                    Case 8
+                        Me.PictureBox1.ImageLocation = arg
+                    Case 9
+                        duration = arg
+                        If pause = "Pause.png" And duration <> "0:00" Then
+                            AxWindowsMediaPlayer1.URL = url
+                            AxWindowsMediaPlayer1.Ctlcontrols.currentPosition = position
+                            AxWindowsMediaPlayer1.Ctlcontrols.play()
+                        Else
+                            AxWindowsMediaPlayer1.Ctlcontrols.pause()
+                        End If
+                End Select
+            Next
+        End If
+        'if the app is minimized then restore it
+        If Me.WindowState = FormWindowState.Minimized Then Me.WindowState = FormWindowState.Normal
+        'if the app is in the background then bring it to the foreground
+        'e.BringToForeground = False
+    End Sub
+End Class
